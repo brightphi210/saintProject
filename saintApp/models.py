31c457b36ad0dc9e00ac573a062ef1079e90c710
Bright Philip <chibuzorphilip2001@gmail.com>
2024-09-26 21:42:20 +0100

    Latest

--- saintApp/models.py
+++ saintApp/models.py
@@ -23,7 +23,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=255, blank=True, null=True)
     is_active = models.BooleanField(default=False)
-    is_staff = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
     last_login = models.DateTimeField(null=True, blank=True)
